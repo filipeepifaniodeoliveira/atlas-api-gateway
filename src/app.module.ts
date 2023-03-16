02e5c53feb54921f247124dea4801d9508ba9f1a
filipeepifaniodeoliveira <filipeepifanio1991@gmail.com>
2023-03-15 23:27:11 -0300

    Test builder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { FinancesModule } from './finances/finances.module';
 import { NewsModule } from './news/news.module';
+import { env } from 'process';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://filipeepifaniooliveira:Vagalumes@arenasport.p1ccroc.mongodb.net/arena?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(env.DATABASE_URL),
     UsersModule,
     FinancesModule,
     NewsModule,
